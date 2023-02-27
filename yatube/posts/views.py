2f322e320e6afb986487fdf82495eecb40249922
Андриянов Иван <poke37454@yandex.ru>
2023-02-27 23:46:42 +0300

    123

---
+++ yatube/posts/views.py
@@ -0,0 +1,79 @@
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.decorators.csrf import csrf_exempt
+
+from .forms import PostForm
+from .models import Group, Post, User
+
+
+def page_look(post_list, request):
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return page_obj
+
+
+def index(request):
+    post_list = Post.objects.select_related('author')
+    page_obj = page_look(post_list, request)
+    context = {
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    post_list = group.posts.all()
+    page_obj = page_look(post_list, request)
+    context = {
+        'group': group,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    author_posts = author.posts.all()
+    page_obj = page_look(author_posts, request)
+    context = {
+        'author': author,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    context = {
+        'posts': post,
+    }
+    return render(request, 'posts/post_detail.html', context)
+
+
+@login_required
+@csrf_exempt
+def post_create(request):
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect('posts:profile', post.author.username)
+    return render(request, 'posts/create_post.html', {'form': form})
+
+
+@login_required
+@csrf_exempt
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    form = PostForm(request.POST, instance=post)
+    if form.is_valid():
+        post.save()
+        return redirect('posts:post_detail', post_id)
+    context = {
+        'form': form,
+    }
+    return render(request, 'posts/create_post.html', context)
